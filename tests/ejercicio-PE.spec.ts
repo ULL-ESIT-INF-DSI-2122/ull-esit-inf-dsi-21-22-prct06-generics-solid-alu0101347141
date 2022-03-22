7813b180df1893e1f7be3be10e99f95d4020e10a
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-22 11:42:16 +0000

    Inicializado proyecto

---
+++ tests/ejercicio-PE.spec.ts
@@ -0,0 +1,11 @@
+/*
+import 'mocha';
+import {expect} from 'chai';
+import {clase} from '../src/ejercicio-PE';
+
+describe('Prueba', () => {
+  it('Existe la prueba', () => {
+    expect().to.exist;
+  });
+});
+*/
