22d62d828b86929edb820d44531f7547989c2a6e
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-26 12:29:43 +0000

    Almacena nombre, altura y peso (rasgos basicos)

--- src/ejercicio-1/fighter.ts
+++ src/ejercicio-1/fighter.ts
@@ -2,4 +2,12 @@
  * Clase para representar los rasgos básicos de un luchador
  */
 export abstract class Fighter {
+/**
+ *
+ * @param {string} nombre
+ * @param {number} altura
+ * @param {number} peso
+ */
+  constructor(protected nombre: string, protected altura: number, protected peso: number) {
+  }
 }
