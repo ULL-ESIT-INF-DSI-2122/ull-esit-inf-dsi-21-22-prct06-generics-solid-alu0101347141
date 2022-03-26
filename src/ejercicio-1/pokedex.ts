8e069aac20a774c2bc4e301d38d14b38213399a6
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-26 17:56:22 +0000

    Adaptada la clase Pokedex y comprobada

--- src/ejercicio-1/pokedex.ts
+++ src/ejercicio-1/pokedex.ts
@@ -1,5 +1,45 @@
+import {Marvel} from './marvel';
+import {DC} from './dc';
+import {StarWars} from './starwars';
+import {DragonBall} from './dragonball';
+
+type Universo = Marvel | DC | StarWars | DragonBall;
+
 /**
  * Clase de Pokedex universal (admite personajes de todos los universos)
  */
 export class Pokedex {
+  /**
+   * Constructor de la clase Pokedex
+   */
+  constructor(protected elementos: Universo[]) {}
+  /**
+   * Función para obtener la base de datos almacenada en la Pokedex
+   * @return {Universo[]} Devuelve la lista de Pokemon almacenada en la Pokedex
+   */
+  getLista(): Universo[] {
+    return this.elementos;
+  }
+  /**
+     * Función para añadir uno o varios Pokemon a la base de datos de la Pokedex
+     * @param {[Universo, ...Universo[]]} elementos
+     */
+  añadir(elementos: [Universo, ...Universo[]]) {
+    for (let i: number = 0; i < elementos.length; i++) {
+      this.elementos.push(elementos[i]);
+    }
+  }
+  /**
+     * Función para eliminar a uno o varios Pokemon de la base de datos de la Pokedex
+     * @param {[string, ...string[]]} elementos
+     */
+  borrar(elementos: [string, ...string[]]) {
+    for (let i: number = 0; i < this.elementos.length; i++) {
+      for (let j: number = 0; j < elementos.length; j++) {
+        if (this.elementos[i].getNombre() == elementos[j]) {
+          this.elementos.splice(i, 1);
+        }
+      }
+    }
+  }
 }
--- tests/ejercicio-1/pokedex.spec.ts
+++ tests/ejercicio-1/pokedex.spec.ts
@@ -1,9 +1,35 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Pokedex} from '../../src/ejercicio-1/pokedex';
+import {Marvel} from '../../src/ejercicio-1/marvel';
+import {DC} from '../../src/ejercicio-1/dc';
+import {StarWars} from '../../src/ejercicio-1/starwars';
+import {DragonBall} from '../../src/ejercicio-1/dragonball';
 
-describe('Pokedex', () => {
+describe('Pokedex Universal', () => {
   it('Debe existir una clase Pokedex universal', () => {
     expect(Pokedex).to.exist;
+  });
+  const hulk = new Marvel('Hulk', 3.00, 635.0, 100, 90, 30, 10000, 'Bruce Banner', ['Abominación', 'Thunderbolt Ross'],
+      ['fuerza', 'radiacion'], ['Vengadores', 'Defensores'], ['¡¡HULK APLASTA!!', 'AAAAGRRGRR', 'GGRRRRGRR']);
+  const goku = new DragonBall('Goku', 1.80, 89.2, 90, 60, 98, 560, 'Planeta Vegeta', 'saiyajin',
+      ['Mono Gigante', 'Supersaiyajin'], ['Gogeta', 'Gotán', 'Kuhan'], ['¡No me rendiré!', 'Kame...hame...HAAA', '¡Alzad las manos!']);
+  const batman = new DC('Batman', 1.88, 95, 40, 50, 80, 300, 'Bruce Wayne', ['Joker', 'Enigma', 'Dos Caras'],
+      ['gadgets', 'intelecto', 'sigilo'], true, ['Soy Batman.']);
+  const kenobi = new StarWars('Obi-Wan Kenobi', 1.82, 80, 60, 50, 60, 300, 'Stewjon', 'humano',
+      true, ['Maestro Jedi', 'Miembro del Consejo', 'General Jedi'], ['Que la fuerza no te acompañe']);
+  const thor = new Marvel('Thor', 1.98, 290.3, 90, 65, 50, 800, 'Thor Odinson', ['Loki', 'Hela'],
+      ['electricidad', 'mjolnir', 'viento'], ['Vengadores'], ['¡POR ASGARD!']);
+  const pokedexUniversal = new Pokedex([hulk, goku, batman, kenobi]);
+  it('Una pokedex universal puede contener elementos de todos los universos', () => {
+    expect(pokedexUniversal.getLista()).to.deep.equal([hulk, goku, batman, kenobi]);
+  });
+  it('Se pueden añadir personajes', () => {
+    pokedexUniversal.añadir([thor]);
+    expect(pokedexUniversal.getLista()).to.deep.equal([hulk, goku, batman, kenobi, thor]);
+  });
+  it('Se pueden borrar personajes', () => {
+    pokedexUniversal.borrar(['Batman']);
+    expect(pokedexUniversal.getLista()).to.deep.equal([hulk, goku, kenobi, thor]);
   });
 });
