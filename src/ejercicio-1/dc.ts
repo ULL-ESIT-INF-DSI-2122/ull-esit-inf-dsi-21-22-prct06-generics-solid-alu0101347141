0409bbcd02a2606807739e8367bc766032ebf7d3
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-26 13:37:52 +0000

    Comprobada clase DC, pasa las pruebas

---
+++ src/ejercicio-1/dc.ts
@@ -0,0 +1,51 @@
+import {Fighter} from './fighter';
+
+/**
+ * Clase para representar un luchador de DC
+ */
+export class DC extends Fighter {
+/**
+ *
+ * @param {string} nombre
+ * @param {number} altura
+ * @param {number} peso
+ * @param {number} ataque
+ * @param {number} defensa
+ * @param {number} velocidad
+ * @param {number} hp
+ */
+  constructor(nombre: string, altura: number, peso: number,
+      ataque: number, defensa: number, velocidad: number, hp: number,
+      protected nombreReal: string, protected nemesis: string[],
+      protected poderes: string[], protected activo: boolean) {
+    super(nombre, 'dc', altura, peso, ataque, defensa, velocidad, hp);
+  }
+  /**
+   * Getter de nombreReal
+   * @return {string}
+   */
+  getNombreReal(): string {
+    return this.nombreReal;
+  }
+  /**
+   * Getter de nemesis
+   * @return {string[]}
+   */
+  getNemesis(): string[] {
+    return this.nemesis;
+  }
+  /**
+   * Getter de poderes
+   * @return {string}
+   */
+  getPoderes(): string[] {
+    return this.poderes;
+  }
+  /**
+   * Getter de activo
+   * @return {boolean}
+   */
+  isActive(): boolean {
+    return this.activo;
+  }
+}
---
+++ tests/ejercicio-1/dc.spec.ts
@@ -0,0 +1,54 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Fighter} from '../../src/ejercicio-1/fighter';
+import {DC} from '../../src/ejercicio-1/dc';
+
+describe('DC', () => {
+  it('Debe existir una clase DC', () => {
+    expect(DC).to.exist;
+  });
+  const batman = new DC('Batman', 1.88, 95, 40, 50, 80, 300, 'Bruce Wayne', ['Joker', 'Enigma', 'Dos Caras'], ['gadgets', 'intelecto', 'sigilo'], true);
+  it('Una instancia de DC debe ser una instancia de Fighter', () => {
+    expect(batman instanceof Fighter).to.be.equal(true);
+  });
+  describe('Debe poder almacenar los siguientes atributos (Fighter)', () => {
+    it('- Nombre', () => {
+      expect(batman.getNombre()).to.be.equal('Batman');
+    });
+    it('- Universo', () => {
+      expect(batman.getUniverso()).to.be.equal('dc');
+    });
+    it('- Altura', () => {
+      expect(batman.getAltura()).to.be.equal(1.88);
+    });
+    it('- Peso', () => {
+      expect(batman.getPeso()).to.be.equal(95);
+    });
+    it('- Ataque', () => {
+      expect(batman.getAtaque()).to.be.equal(40);
+    });
+    it('- Defensa', () => {
+      expect(batman.getDefensa()).to.be.equal(50);
+    });
+    it('- Velocidad', () => {
+      expect(batman.getVelocidad()).to.be.equal(80);
+    });
+    it('- HP', () => {
+      expect(batman.getHp()).to.be.equal(300);
+    });
+  });
+  describe('Debe poder almacenar los siguientes atributos específicos', () => {
+    it('- Nombre Real', () => {
+      expect(batman.getNombreReal()).to.be.equal('Bruce Wayne');
+    });
+    it('- Nemesis', () => {
+      expect(batman.getNemesis()).to.deep.equal(['Joker', 'Enigma', 'Dos Caras']);
+    });
+    it('- Poderes', () => {
+      expect(batman.getPoderes()).to.deep.equal(['gadgets', 'intelecto', 'sigilo']);
+    });
+    it('- Afiliaciones', () => {
+      expect(batman.isActive()).to.be.equal(true);
+    });
+  });
+});
