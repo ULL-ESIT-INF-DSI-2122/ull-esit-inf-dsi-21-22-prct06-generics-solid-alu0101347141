ab524b838f0fff17c2479d39b78dae6c6c1b2c40
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-26 14:13:56 +0000

    Fallan los getters específicos, los implementamos (y los atributos)

--- src/ejercicio-1/dragonball.ts
+++ src/ejercicio-1/dragonball.ts
@@ -4,4 +4,20 @@ import {Fighter} from './fighter';
  * Clase para representar un luchador de Star Wars
  */
 export class DragonBall extends Fighter {
+  /**
+ *
+ * @param {string} nombre
+ * @param {number} altura
+ * @param {number} peso
+ * @param {number} ataque
+ * @param {number} defensa
+ * @param {number} velocidad
+ * @param {number} hp
+ */
+  constructor(nombre: string, altura: number, peso: number,
+      ataque: number, defensa: number, velocidad: number, hp: number,
+    protected planetaNatal: string, protected raza: string,
+    protected portadorFuerza: boolean, protected titulos: string[]) {
+    super(nombre, 'dragonball', altura, peso, ataque, defensa, velocidad, hp);
+  }
 }
