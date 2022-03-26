4868e40b27b36fc1fdaff6694f347845e150e49a
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-26 12:38:15 +0000

    Implementados los getters

--- src/ejercicio-1/fighter.ts
+++ src/ejercicio-1/fighter.ts
@@ -8,9 +8,68 @@ export abstract class Fighter {
  * @param {string} universo
  * @param {number} altura
  * @param {number} peso
- *
+ * @param {number} ataque
+ * @param {number} defensa
+ * @param {number} velocidad
+ * @param {number} hp
  */
-  constructor(protected nombre: string, protected universo: string, protected altura: number, protected peso: number,
+  constructor(protected readonly nombre: string, protected readonly universo: string, protected altura: number, protected peso: number,
   protected ataque: number, protected defensa: number, protected velocidad: number, protected hp: number) {
+  }
+  /**
+   * Getter de nombre
+   * @return {string}
+   */
+  getNombre(): string {
+    return this.nombre;
+  }
+  /**
+   * Getter de universo
+   * @return {string}
+   */
+  getUniverso(): string {
+    return this.universo;
+  }
+  /**
+   * Getter de altura
+   * @return {number}
+   */
+  getAltura(): number {
+    return this.altura;
+  }
+  /**
+   * Getter de peso
+   * @return {number}
+   */
+  getPeso(): number {
+    return this.peso;
+  }
+  /**
+   * Getter de ataque
+   * @return {number}
+   */
+  getAtaque(): number {
+    return this.ataque;
+  }
+  /**
+   * Getter de defensa
+   * @return {number}
+   */
+  getDefensa(): number {
+    return this.defensa;
+  }
+  /**
+   * Getter de velocidad
+   * @return {number}
+   */
+  getVelocidad(): number {
+    return this.velocidad;
+  }
+  /**
+   * Getter de HP
+   * @return {number}
+   */
+  getHp(): number {
+    return this.hp;
   }
 }
