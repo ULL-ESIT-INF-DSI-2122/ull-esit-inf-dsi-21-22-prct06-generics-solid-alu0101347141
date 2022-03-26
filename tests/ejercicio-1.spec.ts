01c087f910af9d8c57352496f3dea38ea948f213
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-26 12:45:39 +0000

    Debe existir una clase Marvel

--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -1,9 +1,16 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Fighter} from '../src/ejercicio-1/fighter';
+import {Marvel} from '../src/ejercicio-1/marvel';
 
 describe('Fighter', () => {
   it('Debe existir una clase abstracta para representar luchadores', () => {
     expect(Fighter).to.exist;
+  });
+});
+
+describe('Marvel', () => {
+  it('Debe existir una clase Marvel que extienda Fighter', () => {
+    expect(Marvel).to.exist;
   });
 });
