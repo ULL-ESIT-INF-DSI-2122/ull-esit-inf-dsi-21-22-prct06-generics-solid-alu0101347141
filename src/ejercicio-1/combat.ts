d501193c762ff9d0b21e5949a4105af074501da4
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-26 16:05:46 +0000

    Implementado atributo que almacena dos adversarios

--- src/ejercicio-1/combat.ts
+++ src/ejercicio-1/combat.ts
@@ -3,8 +3,22 @@ import {DC} from './dc';
 import {StarWars} from './starwars';
 import {DragonBall} from './dragonball';
 
+type Universo = Marvel | DC | StarWars | DragonBall;
+
 /**
  * Clase para simular un combate
  */
 export class Combat {
+  /**
+   * Constructor de la clase Combat
+   * @param {[Universo, Universo]} adversarios
+   */
+  constructor(protected adversarios: [Universo, Universo]) {};
+  /**
+   * Getter de ambos adversarios
+   * @return {[Universo, Universo]}
+   */
+  getAdversarios(): [Universo, Universo] {
+    return this.adversarios;
+  }
 }
--- tests/ejercicio-1/combat.spec.ts
+++ tests/ejercicio-1/combat.spec.ts
@@ -1,9 +1,20 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Combat} from '../../src/ejercicio-1/combat';
+import {Marvel} from '../../src/ejercicio-1/marvel';
+// import {DC} from '../../src/ejercicio-1/dc';
+// import {StarWars} from '../../src/ejercicio-1/starwars';
+import {DragonBall} from '../../src/ejercicio-1/dragonball';
 
 describe('Combat', () => {
   it('Debe existir una clase Combat', () => {
     expect(Combat).to.exist;
+  });
+  const hulk = new Marvel('Hulk', 3.00, 635.0, 100, 90, 30, 10000, 'Bruce Banner', ['Abominación', 'Thunderbolt Ross'], ['fuerza', 'radiacion'], ['Vengadores', 'Defensores']);
+  const goku = new DragonBall('Goku', 1.80, 89.2, 90, 60, 98, 560, 'Planeta Vegeta', 'saiyajin', ['Mono Gigante', 'Supersaiyajin'], ['Gogeta', 'Yamta']);
+  const combate = new Combat([hulk, goku]);
+  it('Almacena dos adversarios de cualquier universo', () => {
+    expect(combate.getAdversarios()[0]).to.deep.equal(hulk);
+    expect(combate.getAdversarios()[1]).to.deep.equal(goku);
   });
 });
