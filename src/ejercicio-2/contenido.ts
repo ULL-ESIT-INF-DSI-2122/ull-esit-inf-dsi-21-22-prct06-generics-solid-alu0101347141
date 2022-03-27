419bed48f3c66a23746ac80c4ec64acbd7f22dfe
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-27 00:16:38 +0000

    Creada clase para representar un Documental

--- src/ejercicio-2/contenido.ts
+++ src/ejercicio-2/contenido.ts
@@ -35,3 +35,22 @@ export class Serie {
     private productor: string, private guionistas: string[], private reparto: string[],
     private generos: string[]) {}
 }
+
+/**
+ * Clase para representar un documental
+ */
+export class Documental {
+  /**
+   * Constructor de la clase Documental
+   * @param {string} nombre
+   * @param {number[]} episodios // Cada elemento tendrá la cantidad de episodios por temporada
+   * @param {string} fecha
+   * @param {string} productor
+   * @param {string} narrador
+   * @param {string[]} reparto
+   * @param {string[]} temas
+   */
+  constructor(private nombre: string, private episodios: number[], private fecha: string,
+    private productor: string, private narrador: string, private reparto: string[],
+    private temas: string[]) {}
+}
