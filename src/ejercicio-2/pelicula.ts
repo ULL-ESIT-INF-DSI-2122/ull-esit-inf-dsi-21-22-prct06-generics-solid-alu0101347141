cb6f889dc8c69ba8dda682f26fea791f0346d587
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-27 00:44:48 +0000

    Implementados getter básicos de Serie

---
+++ src/ejercicio-2/pelicula.ts
@@ -0,0 +1,69 @@
+/**
+ * Clase para representar una película
+ */
+export class Pelicula {
+  /**
+   * Constructor de la clase Pelicula
+   * @param {string} nombre
+   * @param {number} duracion
+   * @param {string} fecha
+   * @param {string} director
+   * @param {string} guionista
+   * @param {string[]} reparto
+   * @param {string[]} generos
+   */
+  constructor(private nombre: string, private duracion: number, private fecha: string,
+    private director: string, private guionista: string, private reparto: string[],
+    private generos: string[]) {}
+
+  /**
+     * Getter de nombre
+     * @return {string}
+     */
+  getNombre(): string {
+    return this.nombre;
+  }
+  /**
+     * Getter de duracion
+     * @return {number}
+     */
+  getDuracion(): number {
+    return this.duracion;
+  }
+  /**
+     * Getter de fecha
+     * @return {string}
+     */
+  getFecha(): string {
+    return this.fecha;
+  }
+  /**
+     * Getter de director
+     * @return {string}
+     */
+  getDirector(): string {
+    return this.director;
+  }
+  /**
+     * Getter de guionista
+     * @return {string}
+     */
+  getGuionista(): string {
+    return this.guionista;
+  }
+  /**
+     * Getter de reparto
+     * @return {string[]}
+     */
+  getReparto(): string[] {
+    return this.reparto;
+  }
+  /**
+     * Getter de generos
+     * @return {string[]}
+     */
+  getGeneros(): string[] {
+    return this.generos;
+  }
+}
+
---
+++ src/ejercicio-2/serie.ts
@@ -0,0 +1,80 @@
+/**
+ * Clase para representar una serie
+ */
+export class Serie {
+  /**
+   * Constructor de la clase Serie
+   * @param {string} nombre
+   * @param {number[]} episodios // Cada elemento tendrá la cantidad de episodios por temporada
+   * @param {string} fecha
+   * @param {string} productor
+   * @param {string[]} guionistas
+   * @param {string[]} reparto
+   * @param {string[]} generos
+   */
+  constructor(private nombre: string, private episodios: number[], private fecha: string,
+    private productor: string, private guionistas: string[], private reparto: string[],
+    private generos: string[]) {}
+
+  /**
+     * Getter de nombre
+     * @return {string}
+     */
+  getNombre(): string {
+    return this.nombre;
+  }
+  /**
+     * Getter de cantidad de episodios por temporada
+     * @return {number[]}
+     */
+  getEpisodiosPorTemporada(): number[] {
+    return this.episodios;
+  }
+  /**
+     * Getter de cantidad de episodios totales
+     * @return {number}
+     */
+  getEpisodiosTotales(): number {
+    let total: number = 0;
+    this.episodios.forEach(function(a) {
+      total += a;
+    });
+    return total;
+  }
+  /**
+     * Getter de fecha
+     * @return {string}
+     */
+  getFecha(): string {
+    return this.fecha;
+  }
+  /**
+     * Getter de productor
+     * @return {string}
+     */
+  getProductor(): string {
+    return this.productor;
+  }
+  /**
+     * Getter de guionistas
+     * @return {string[]}
+     */
+  getGuionista(): string[] {
+    return this.guionistas;
+  }
+  /**
+     * Getter de reparto
+     * @return {string[]}
+     */
+  getReparto(): string[] {
+    return this.reparto;
+  }
+  /**
+     * Getter de generos
+     * @return {string[]}
+     */
+  getGeneros(): string[] {
+    return this.generos;
+  }
+}
+
