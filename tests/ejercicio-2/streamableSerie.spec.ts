36bca88d586f1a6a3a4883302d633f910deec17f
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-27 01:56:20 +0000

    Se puede borrar y añadir una serie (pasa las pruebas)

--- tests/ejercicio-2/streamableSerie.spec.ts
+++ tests/ejercicio-2/streamableSerie.spec.ts
@@ -13,8 +13,18 @@ describe('ColeccionSeries', () => {
       ['Fantasía', 'Drama', 'Aventura'], 9.2);
   const serie3 = new Serie('Juego del Calamar', [9], '17/9/2021', 'Hwang Dong-Hyuk', ['Hoyeon Jung', 'Lee Jung-jae', 'Gong Yoo'],
       ['Suspense', 'Supervivencia', 'Drama'], 9.2);
+  const serie4 = new Serie('The Mandalorian', [8, 8], '12/11/2019', 'Jon Favreu', ['Pedro Pascal'],
+      ['Space western', 'Ciencia ficción', 'Fantasía'], 9.2);
   const coleccionTest = new ColeccionSeries([serie1, serie2, serie3]);
   it('Debe almacenar una colección de series', () => {
+    expect(coleccionTest.getColeccion()).to.deep.equal([serie1, serie2, serie3]);
+  });
+  it('Se puede añadir una nueva serie', () => {
+    coleccionTest.añadir(serie4);
+    expect(coleccionTest.getColeccion()).to.deep.equal([serie1, serie2, serie3, serie4]);
+  });
+  it('Se puede eliminar una serie', () => {
+    coleccionTest.borrar(3);
     expect(coleccionTest.getColeccion()).to.deep.equal([serie1, serie2, serie3]);
   });
 });
