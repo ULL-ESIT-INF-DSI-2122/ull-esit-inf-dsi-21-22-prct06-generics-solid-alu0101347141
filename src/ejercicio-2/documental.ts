d85fb7dc8807e841badb8f8e0549c6c1441ad744
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-27 00:46:23 +0000

    Implementados getter básicos de Documental

--- src/ejercicio-2/documental.ts
+++ src/ejercicio-2/documental.ts
@@ -13,6 +13,59 @@ export class Documental {
    * @param {string[]} temas
    */
   constructor(private nombre: string, private episodios: number[], private fecha: string,
-    private productor: string, private narrador: string, private reparto: string[],
-    private temas: string[]) {}
+    private productor: string, private narrador: string, private temas: string[]) {}
+  /**
+     * Getter de nombre
+     * @return {string}
+     */
+  getNombre(): string {
+    return this.nombre;
+  }
+  /**
+     * Getter de cantidad de episodios por temporada
+     * @return {number[]}
+     */
+  getEpisodiosPorTemporada(): number[] {
+    return this.episodios;
+  }
+  /**
+     * Getter de cantidad de episodios totales
+     * @return {number}
+     */
+  getEpisodiosTotales(): number {
+    let total: number = 0;
+    this.episodios.forEach(function(a) {
+      total += a;
+    });
+    return total;
+  }
+  /**
+     * Getter de fecha
+     * @return {string}
+     */
+  getFecha(): string {
+    return this.fecha;
+  }
+  /**
+     * Getter de productor
+     * @return {string}
+     */
+  getProductor(): string {
+    return this.productor;
+  }
+  /**
+     * Getter de narrador
+     * @return {string}
+     */
+  getGuionista(): string {
+    return this.narrador;
+  }
+  /**
+     * Getter de temas
+     * @return {string[]}
+     */
+  getTemas(): string[] {
+    return this.temas;
+  }
 }
+
