b461b3c577eec8dd37788bf72258f5b778a71891
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-27 02:36:07 +0000

    Debe existir una clase para una colección de peliculas

--- tests/ejercicio-2/streamablePelicula.spec.ts
+++ tests/ejercicio-2/streamablePelicula.spec.ts
@@ -1,1 +1,10 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Pelicula} from '../../src/ejercicio-2/pelicula';
+import {ColeccionPeliculas} from '../../src/ejercicio-2/coleccionPeliculas';
 
+describe('ColeccionSeries', () => {
+  it('Debe existir una clase ColeccionSeries', () => {
+    expect(ColeccionPeliculas).to.exist;
+  });
+});
